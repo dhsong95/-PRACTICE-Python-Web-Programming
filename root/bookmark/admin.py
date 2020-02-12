8ab653cf8bc34d39a07919d31fecfecf7fa80581
Daehoon Song <dhsong@Song-MacBookPro.local>
2020-02-12 18:02:52 +0900

    Basic Bookmark Application

--- root/bookmark/admin.py
+++ root/bookmark/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Bookmark
+
+
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ['title', 'url']
+
 
 # Register your models here.
+admin.site.register(Bookmark, BookmarkAdmin)
--- root/bookmark/views.py
+++ root/bookmark/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .models import Bookmark
+
 
 # Create your views here.
+class BookmarkLV(ListView):
+    model = Bookmark
+
+
+class BookmarkDV(DetailView):
+    model = Bookmark
